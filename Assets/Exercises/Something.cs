2a74468b5e096728c23f6fb6fbe1aeccdefe574d
Aniol <aniol92@hotmail.com>
2025-05-29 12:24:36 +0200

    First commit

---
+++ Assets/Exercises/Something.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Something : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    
+
+    private void LateUpdate()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
